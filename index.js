a6bace2045e424fda56245aea4b88dcf7c409bc7
lucasfarmwald <farmwaldlucas@gmail.com>
2021-09-20 11:03:46 -0500

    Done.

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ element.append(ul);
 //function newHeader(){
   const newHeader = document.createElement("h1")
    newHeader.id = 'victory'
-      newHeader.innerHTML = 'is the champion';
+      newHeader.innerHTML = 'is the champion'; 
 //}
 
