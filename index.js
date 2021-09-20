a737e63b954f5bdfe70a674a72a41a004e4b2f1e
lucasfarmwald <farmwaldlucas@gmail.com>
2021-09-20 10:17:21 -0500

    Done.

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ for (let i = 0; i < 3; i++) {
   const li = document.createElement("li");
   li.textContent = (i + 1).toString();
   ul.append(li);
-}
 
 element.append(ul);
+}
+//function newHeader(){
+  let element = document.createElement("<h1>")
+   element.id = 'victory'
+  //main.innerHTML = '<h1> victory </h1>';
+}
